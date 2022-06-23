fd55c07155b41633e68351c27f0b04b4c9016c40
Zen <muhzaini30@gmail.com>
2022-06-23 19:00:32 +0800

    style: prettier

--- docs/lib/elemenBermasalah.js
+++ docs/lib/elemenBermasalah.js
@@ -1,8 +1,11 @@
-function init(){
-  let elemenBermasalah = document.querySelector('.sidebar-nav li.active')
-  elemenBermasalah.innerHTML = elemenBermasalah.innerHTML.replace('undefined', '')
+function init() {
+  let elemenBermasalah = document.querySelector(".sidebar-nav li.active");
+  elemenBermasalah.innerHTML = elemenBermasalah.innerHTML.replace(
+    "undefined",
+    ""
+  );
 }
-setTimeout(init, 500)
-setTimeout(init, 1000)
-setTimeout(init, 1500)
-setTimeout(init, 2000)
+setTimeout(init, 500);
+setTimeout(init, 1000);
+setTimeout(init, 1500);
+setTimeout(init, 2000);
