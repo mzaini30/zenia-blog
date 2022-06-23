671521490bddb76b3e9df216b14c327dbdcda0ee
Zen <muhzaini30@gmail.com>
2022-06-24 01:12:45 +0800

    chore: upload

--- script/injectMeta.ts
+++ script/injectMeta.ts
@@ -6,8 +6,7 @@ let link = [...semuaFile].map((x) =>
   x.replace("index.html", "").replace(".presite", "https://zenia.my.id")
 );
 
-let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+let sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 for (let x of link) {
   sitemap += `<url><loc>${x}</loc></url>`;
 }
