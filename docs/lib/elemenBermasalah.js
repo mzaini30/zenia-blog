1d513d6d962f5b4e8a747999d473d9e311676ace
Zen <muhzaini30@gmail.com>
2022-06-23 19:27:57 +0800

    style: prettier

--- docs/lib/elemenBermasalah.js
+++ docs/lib/elemenBermasalah.js
@@ -1,14 +1,18 @@
 function init() {
   let elemenBermasalah = document.querySelector(".sidebar-nav li.active");
-  if (elemenBermasalah){
+  if (elemenBermasalah) {
     elemenBermasalah.innerHTML = elemenBermasalah.innerHTML.replace(
       "undefined",
       ""
     );
   }
 
-  document.querySelector('div.search:nth-child(2)').style.display = 'none'
-  document.querySelector('div.search:nth-child(1) > div:nth-child(1) > input:nth-child(1)').setAttribute('placeholder', 'Cari apa?')
+  document.querySelector("div.search:nth-child(2)").style.display = "none";
+  document
+    .querySelector(
+      "div.search:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+    )
+    .setAttribute("placeholder", "Cari apa?");
 }
 setTimeout(init, 500);
 setTimeout(init, 1000);
