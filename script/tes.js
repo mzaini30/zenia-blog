02113871335a70df807d7038f997df44c3167bd4
Muhammad Zaini <muhzaini30@gmail.com>
2022-06-24 01:22:48 +0800

    Delete tes.js

--- script/tes.js
+++
@@ -1,2 +0,0 @@
-let masalahUndefined = document.querySelector('.sidebar-nav li.active').innerHTML
-document.querySelector('.sidebar-nav li.active').innerHTML = masalahUndefined.replace('undefined', '')
