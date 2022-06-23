36f14963016d3bbb3b8476e5d97ebe1c3c26ed99
Zen <muhzaini30@gmail.com>
2022-06-23 19:00:16 +0800

    style: prettier

--- script/injectAds.ts
+++ script/injectAds.ts
@@ -1,9 +1,12 @@
-import {sync} from 'fast-glob'
-import {readFileSync, writeFileSync} from 'fs'
+import { sync } from "fast-glob";
+import { readFileSync, writeFileSync } from "fs";
 
-const semuaFile = sync('.presite/**/*.html')
-for (let x of semuaFile){
-  let isi = readFileSync(x).toString()
-  isi = isi.replace('<head>', `<head><script async='async' crossorigin='anonymous' src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2408628281705149'></script>`)
-  writeFileSync(x, isi)
+const semuaFile = sync(".presite/**/*.html");
+for (let x of semuaFile) {
+  let isi = readFileSync(x).toString();
+  isi = isi.replace(
+    "<head>",
+    `<head><script async='async' crossorigin='anonymous' src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2408628281705149'></script>`
+  );
+  writeFileSync(x, isi);
 }
