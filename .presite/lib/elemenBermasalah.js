a6dd2cdf82ee901bbf39b4d5df4c5fd04b5ad572
Zen <muhzaini30@gmail.com>
2022-06-23 19:27:44 +0800

    fix: cari elemen bermasalah

--- .presite/lib/elemenBermasalah.js
+++ .presite/lib/elemenBermasalah.js
@@ -1,9 +1,14 @@
 function init() {
   let elemenBermasalah = document.querySelector(".sidebar-nav li.active");
-  elemenBermasalah.innerHTML = elemenBermasalah.innerHTML.replace(
-    "undefined",
-    ""
-  );
+  if (elemenBermasalah){
+    elemenBermasalah.innerHTML = elemenBermasalah.innerHTML.replace(
+      "undefined",
+      ""
+    );
+  }
+
+  document.querySelector('div.search:nth-child(2)').style.display = 'none'
+  document.querySelector('div.search:nth-child(1) > div:nth-child(1) > input:nth-child(1)').setAttribute('placeholder', 'Cari apa?')
 }
 setTimeout(init, 500);
 setTimeout(init, 1000);
