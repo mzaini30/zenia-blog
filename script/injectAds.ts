07aad9ad9af9a762d1c8ce5ff6bb31645a5f3e20
Zen <muhzaini30@gmail.com>
2022-06-24 07:10:47 +0800

    feat: tambah giscus

--- script/injectAds.ts
+++ script/injectAds.ts
@@ -4,9 +4,11 @@ import { readFileSync, writeFileSync } from "fs";
 const semuaFile = sync(".presite/**/*.html");
 for (let x of semuaFile) {
   let isi = readFileSync(x).toString();
-  isi = isi.replace(
-    "<head>",
-    `<head><script async='async' crossorigin='anonymous' src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2408628281705149'></script>`
-  );
+  isi = isi
+    .replace(
+      "<head>",
+      `<head><script async='async' crossorigin='anonymous' src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2408628281705149'></script>`
+    )
+    .replace("</body>", `<script src="/lib/giscus.js"></script></body>`);
   writeFileSync(x, isi);
 }
